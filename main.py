fdb3ab845a162ba6ecadb84fdd202513f60f909d
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-08-14 12:15:03 -0500

    Initial Setup

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def hash_file(directory):
+    print(f'Hi, {directory}')
+
+
+if __name__ == '__main__':
+
+    name = input("Enter the directory you want to rename to hash value [USE C NOTATION] (Example C:/Users/name/Desktop)")
+    hash_file(name)
+
