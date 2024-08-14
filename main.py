0891e829b2090040cd2b7aee15a5eda91752b184
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-08-14 13:48:12 -0500

    hash and rename function works

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 import os
 import hashlib
 
+
 def compute_directory(directory):
     compute_file(directory)
 
 
-def compute_file(file_name):
-    file_name = os.path.basename(file_name)
-    print(file_name)
-    hash_file(file_name)
+def compute_file(src):
+    file_name = os.path.basename(src)
+    print(file_name +"\n"+ src)
+    hash_file(src, file_name)
 
-def hash_file(directory):
-    hasher = hashlib.sha256(directory.encode('utf-8'))
-    print(hasher.hexdigest())
+def get_extension(file_name):
+    split = os.path.splitext(file_name)
+
+
+def hash_file(file_src, old_name):
+    extension = get_extension(old_name)
+
+    hasher = hashlib.sha256()
+    with open(file_src, 'rb') as file_found:
+        while chunk := file_found.read(8192):
+            hasher.update(chunk)
+
+    new_name = hasher.hexdigest()
+    #rename_file(file_src, new_name)
+
+    print(file_src + " "+ old_name + " "+ new_name)
+
+
+def rename_file(old_name, new_name):
+    os.rename(old_name, new_name)
+
 
 if __name__ == '__main__':
 
-    name = input("Enter the directory you want to rename to hash value [USE C NOTATION] (Example C:/Users/name/Desktop):\n")
+    name = input("Enter the directory you want to rename to hash value [USE C NOTATION] "
+                 "(Example C:/Users/name/Desktop):\n")
     compute_directory(name)
 
+
+#C:\Users\miket\Downloads\test.pdf
+#Hash
+# text.pdf
