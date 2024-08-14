0b5c0371c14bd5a3642f4e6b4ec550580dbe1ac0
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-08-14 13:12:40 -0500

    base setup

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import os
+import hashlib
+
+def compute_directory(directory):
+    compute_file(directory)
+
+
+def compute_file(file_name):
+    file_name = os.path.basename(file_name)
+    print(file_name)
+    hash_file(file_name)
 
 def hash_file(directory):
-    print(f'Hi, {directory}')
-
+    hasher = hashlib.sha256(directory.encode('utf-8'))
+    print(hasher.hexdigest())
 
 if __name__ == '__main__':
 
-    name = input("Enter the directory you want to rename to hash value [USE C NOTATION] (Example C:/Users/name/Desktop)")
-    hash_file(name)
+    name = input("Enter the directory you want to rename to hash value [USE C NOTATION] (Example C:/Users/name/Desktop):\n")
+    compute_directory(name)
 
